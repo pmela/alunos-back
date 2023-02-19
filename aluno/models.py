fd49bb1362e9bb64a3250508fe1e0ebe4fcf71e6
pmela <pmelaeduarda2015@gmail.com>
2023-02-19 18:05:28 -0300

    Adiciona Models com Matéria, Nota e Aluno.

--- aluno/models.py
+++ aluno/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Materia(models.Model):
+    descricao = models.CharField(max_length=16)
+
+
+class Nota(models.Model):
+    pontuacao = models.PositiveIntegerField()
+    materia = models.ForeignKey(Materia)
+    aprovado = models.BooleanField(default=False)
+
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=64)
+    cpf = models.CharField(max_length=11)
+    nascimento = models.DateField()
+    nota = models.ManyToManyField(Nota)
